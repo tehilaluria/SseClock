796b7520b1e429a2f3a4b493f18d81c2dd1ad1be
Tehila <tehila055676@gmail.com>
2024-10-09 02:25:57 +0300

    Add project files.

---
+++ SseClock/Controllers/SseController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SseController : ControllerBase
+{
+    [HttpGet("Time")]
+    public async Task GetEvents()
+    {
+        Response.Headers.Add("Content-Type", "text/event-stream");
+        Response.Headers.Add("Cache-Control", "no-cache");
+        Response.Headers.Add("Connection", "keep-alive");
+
+        while (true)
+        {
+            await Response.WriteAsync($"data: {DateTime.Now}\n\n");
+            await Response.Body.FlushAsync();
+            await Task.Delay(1000); 
+        }
+    }
+}
+
+
